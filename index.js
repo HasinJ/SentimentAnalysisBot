6cf2cdb13156b6378e1e6028c3b9e220e5a22713
HasinJ <hasinchoudhuryj@gmail.com>
2020-12-28 03:00:19 -0500

    sets one command in handler

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 require('dotenv').config();
+//const fs = require('fs');
 const Discord = require('discord.js');
+const ping = require('./commands/test.js')
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
+bot.commands = new Discord.Collection();
+
+bot.commands.set("hasin", ping);
 
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
 bot.on('message', msg => {
+  let command = msg.content.toString().substring(1);
 
-  if (msg.content.includes('black')) {
-    let user = bot.users.find("username","Hasin");
-    if(user) msg.channel.send(user.toString());
-    else msg.channel.send("error");
+  if (bot.commands.has(command)) {
+    const args = msg.content.split(/ +/);
+    bot.commands.get(command)(bot,msg,args);
   }
 
-  else if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-  } else if (msg.content.startsWith('!kick')) {
+  else if (msg.content.startsWith('!kick')) {
     if (msg.mentions.users.size) {
       const taggedUser = msg.mentions.users.first();
       msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
